8c6eb0923e23805aa373e9e19226befed02f5756
Patrick Alexander <patrick_alexander@mail.com>
2020-03-31 01:43:37 -0500

    Ruby Controllers and Models added

---
+++ rubyServer/db/migrate/20200331063756_create_thoughts.rb
@@ -0,0 +1,13 @@
+class CreateThoughts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :thoughts do |t|
+      t.string :title
+      t.string :body
+      t.string :tags
+      t.integer :user_id
+      t.datetime :created
+
+      t.timestamps
+    end
+  end
+end
