033a908a2a107f54db5e2e9ba9ae12f073c0699d
Patrick Alexander <patrick_alexander@mail.com>
2020-07-15 23:30:16 -0500

    configred redux-thunk in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import App from './App';
 import reducers from './store/reducers';
 
 ReactDOM.render(
-	<Provider store={createStore(reducers)}>
+	<Provider store={createStore(reducers, applyMiddleware(thunk))}>
 		<App />
 	</Provider>,
 	document.getElementById('root')
--- client/src/store/actions/index.js
+++ client/src/store/actions/index.js
@@ -1,6 +1,13 @@
 import Axios from 'axios';
 
 export const fetchJots = async () => {
+	const config = {
+		'Content-Type': 'application/json',
+		Authorization: `Bearer ${localStorage.token}`
+	};
+
+	const response = await Axios.get('http://localhost:3000/jots', config);
+
 	return {
 		type: 'FETCH_JOTS',
 		payload: response
