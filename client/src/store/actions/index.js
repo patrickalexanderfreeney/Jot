3447d97591793c883008f3a171e7467c208069e8
Patrick Alexander <patrick_alexander@mail.com>
2020-07-15 23:22:58 -0500

    Imported redux-thunk to handle async reqs

--- client/src/store/actions/index.js
+++ client/src/store/actions/index.js
@@ -1,3 +1,11 @@
+import Axios from 'axios';
+
+export const fetchJots = async () => {
+	return {
+		type: 'FETCH_JOTS',
+		payload: response
+	};
+};
 export const selectJot = (jot) => {
 	return {
 		type: 'SELECTED_JOT',
