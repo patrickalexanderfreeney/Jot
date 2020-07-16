a4554cb496d4710fe141ad6ece137ce5f6a0a9b3
Patrick Alexander <patrick_alexander@mail.com>
2020-07-15 23:38:20 -0500

    configured fetchJots to fetch jots for store

--- client/src/store/actions/index.js
+++ client/src/store/actions/index.js
@@ -1,16 +1,15 @@
 import Axios from 'axios';
 
-export const fetchJots = async () => {
-	const config = {
-		'Content-Type': 'application/json',
-		Authorization: `Bearer ${localStorage.token}`
-	};
+export const fetchJots = () => {
+	return async (dispatch) => {
+		const config = {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${localStorage.token}`
+		};
 
-	const response = await Axios.get('http://localhost:3000/jots', config);
+		const response = await Axios.get('http://localhost:3000/jots', config);
 
-	return {
-		type: 'FETCH_JOTS',
-		payload: response
+		dispatch({ type: FETCH_JOTS, payload: response });
 	};
 };
 export const selectJot = (jot) => {
